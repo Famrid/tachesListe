c3afb84d7a5454fa63561184310e291ad5f842db
Lemasson Steven <lemasson.steven@gmail.com>
2023-02-06 13:51:23 +0100

    init project

---
+++ src/main/java/com/example/steven/tachesListe/TachesListeApplication.java
@@ -0,0 +1,13 @@
+package com.example.steven.tachesListe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachesListeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TachesListeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/steven/tachesListe/TachesListeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.steven.tachesListe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TachesListeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
